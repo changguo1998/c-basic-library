8a094c7f580676d40d5146a49e0715ede91091df
Chang Guo <changguo1998@gmail.com>
2024-10-13 21:02:44 -0400

    create header and source file for FloatVector

--- doc/develop_status.md
+++ doc/develop_status.md
@@ -1,41 +1,41 @@
 # Summary
 
-|                       | file       | header   | methods  | test     |
-|-----------------------|------------|----------|----------|----------|
-| type defines          | Basic      | &#x2714; |          |          |
-| basic error           | Basic      | &#x2714; | &#x2714; |          |
-|                       |            |          |          |          |
-| *Basic Type*          |            |          |          |          |
-| String                | String     | &#x2714; | &#x2714; | &#x2714; |
-| Date                  | DateTime   | &#x2714; | &#x2714; | &#x2714; |
-| Time                  | DateTime   | &#x2714; | &#x2714; | &#x2714; |
-| DateTime              | DateTime   | &#x2714; | &#x2714; | &#x2714; |
-| Log                   | Log        | &#x2714; | &#x2714; | &#x2714; |
-| StaticDict            | Dict       | &#x2714; | &#x2714; | &#x2714; |
-| DynamicDict           | Dict       | &#x2714; | &#x2714; | &#x2714; |
-| Sheet                 | Dict       | &#x2714; | &#x2714; | &#x2714; |
-|                       |            |          |          |          |
-| *General Array Type*  |            |          |          |          |
-| Vector                | Vector     |          |          |          |
-| Array                 | Array      |          |          |          |
-| TypeArray Template    |            |          |          |          |
-|                       |            |          |          |          |
-| *Int Arrays*          |            |          |          |          |
-| IntVector             | IntVector  | &#x2714; | &#x2714; | &#x2714; |
-| IntMatrix             |            |          |          |          |
-| IntTensor             |            |          |          |          |
-| IntArray              |            |          |          |          |
-|                       |            |          |          |          |
-| *Float Arrays*        |            |          |          |          |
-| FloatVector           |            |          |          |          |
-| FloatMatrix           |            |          |          |          |
-| FloatTensor           |            |          |          |          |
-| FloatArray            |            |          |          |          |
-|                       |            |          |          |          |
-| *Macro Template*      |            |          |          |          |
-| IntVector_n macro     | IntVectorF |          |          |          |
-| IntMatrix_m_n macro   | IntMatrixF |          |          |          |
-| IntTensor_p_q_r macro |            |          |          |          |
+|                       | file        | header   | methods  | test     |
+|-----------------------|-------------|----------|----------|----------|
+| type defines          | Basic       | &#x2714; |          |          |
+| basic error           | Basic       | &#x2714; | &#x2714; |          |
+|                       |             |          |          |          |
+| *Basic Type*          |             |          |          |          |
+| String                | String      | &#x2714; | &#x2714; | &#x2714; |
+| Date                  | DateTime    | &#x2714; | &#x2714; | &#x2714; |
+| Time                  | DateTime    | &#x2714; | &#x2714; | &#x2714; |
+| DateTime              | DateTime    | &#x2714; | &#x2714; | &#x2714; |
+| Log                   | Log         | &#x2714; | &#x2714; | &#x2714; |
+| StaticDict            | Dict        | &#x2714; | &#x2714; | &#x2714; |
+| DynamicDict           | Dict        | &#x2714; | &#x2714; | &#x2714; |
+| Sheet                 | Dict        | &#x2714; | &#x2714; | &#x2714; |
+|                       |             |          |          |          |
+| *General Array Type*  |             |          |          |          |
+| Vector                | Vector      |          |          |          |
+| Array                 | Array       |          |          |          |
+| TypeArray Template    |             |          |          |          |
+|                       |             |          |          |          |
+| *Int Arrays*          |             |          |          |          |
+| IntVector             | IntVector   | &#x2714; | &#x2714; | &#x2714; |
+| IntMatrix             |             |          |          |          |
+| IntTensor             |             |          |          |          |
+| IntArray              |             |          |          |          |
+|                       |             |          |          |          |
+| *Float Arrays*        |             |          |          |          |
+| FloatVector           | FloatVector | &#x2714; |          |          |
+| FloatMatrix           |             |          |          |          |
+| FloatTensor           |             |          |          |          |
+| FloatArray            |             |          |          |          |
+|                       |             |          |          |          |
+| *Macro Template*      |             |          |          |          |
+| IntVector_n macro     | IntVectorF  |          |          |          |
+| IntMatrix_m_n macro   | IntMatrixF  |          |          |          |
+| IntTensor_p_q_r macro |             |          |          |          |
 
 # Depencency
 
---
+++ include/Type_FloatVector.h
@@ -0,0 +1,171 @@
+/**********************************************************************************
+ * MIT License                                                                    *
+ *                                                                                *
+ * Copyright (c) 2024 Chang Guo                                                   *
+ *                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          *
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in all *
+ * copies or substantial portions of the Software.                                *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                                      *
+ *                                                                                *
+ **********************************************************************************/
+
+#ifndef _CBL_FLOATVECTOR_H_
+#define _CBL_FLOATVECTOR_H_
+
+#include <stdlib.h>
+#include "Module_Basic.h"
+#include "Type_IntVector.h"
+
+/**
+ * @brief FloatVector
+ * @param len Int
+ * @param data Int*
+ */
+struct FloatVector {
+    Int    len;
+    Float* data;
+
+    struct FloatVectorMethods* methods;
+};
+
+struct FloatVectorMethods {
+    // # memory management
+    /* may change the length of the vector */
+
+    /**
+     * @brief free allocated memory
+     * @param this struct FloatVector*
+     */
+    void (*free_)(struct FloatVector* this);
+
+    /**
+     * @brief allocate memory with specified length.\n
+     * If the memory is not allocated, it will allocate memory with given length;\n
+     * If the memory is allocated and the current length is equal to the given length,
+     * nothing will be changed;\n
+     * If the memory is allocated but the current length is not equal to the given length,
+     * it will free the current length, and allocate a new memory with given length.\n
+     * @param this struct FloatVector*
+     * @param len Int
+     */
+    void (*alloc_)(struct FloatVector* this, Int len);
+
+    // # index
+    /**
+     * @brief get value at index, throw error when index is over bound
+     * @param this
+     * @param index Int
+     */
+    Float (*get)(const struct FloatVector* this, Int index);
+
+    // # value setting
+    // # by index
+    void (*index_)(struct FloatVector* this, struct FloatVector src, struct IntVector indexs);
+
+    void (*slice_)(struct FloatVector* this, struct FloatVector src, Int start, Int step, Int stop);
+
+    void (*index_flag_)(struct FloatVector* this, struct FloatVector src, struct IntVector flags);
+
+    void (*set_)(struct FloatVector* this, Int index, Float value);
+
+    // # all
+    void (*rand_)(struct FloatVector* this, Float min, Float max);
+
+    void (*rand_from_)(struct FloatVector* this, struct FloatVector value_set);
+
+    void (*fill_)(struct FloatVector* this, Float value);
+
+    void (*range_)(struct FloatVector* this, Float start, Float step, Float stop);
+
+    void (*copy_from_)(struct FloatVector* this, struct FloatVector src);
+
+    // # methods without changing values in vector
+    Float (*sum)(struct FloatVector* this);
+
+    Float (*prod)(struct FloatVector* this);
+
+    Float (*min)(struct FloatVector* this);
+
+    Float (*max)(struct FloatVector* this);
+
+    Int (*argmin)(struct FloatVector* this);
+
+    Int (*argmax)(struct FloatVector* this);
+
+    // # methods changing values in vector
+    void (*cumsum_)(struct FloatVector* this, Float initial);
+
+    void (*cumprod_)(struct FloatVector* this, Float initial);
+
+    void (*sort_)(struct FloatVector* this);
+
+    void (*sortperm_)(struct FloatVector* this, struct IntVector* perm);
+};
+
+void FloatVector_free_(struct FloatVector* this);
+
+void FloatVector_alloc_(struct FloatVector* this, Int len);
+
+Float FloatVector_get(const struct FloatVector* this, Int index);
+
+void FloatVector_index_(struct FloatVector* this, struct FloatVector src, struct IntVector indexs);
+
+void FloatVector_slice_(struct FloatVector* this, struct FloatVector src, Int start, Int step, Int stop);
+
+void FloatVector_index_flag_(struct FloatVector* this, struct FloatVector src, struct IntVector flags);
+
+void FloatVector_set_(struct FloatVector* this, Int index, Float value);
+
+void FloatVector_rand_(struct FloatVector* this, Float min, Float max);
+
+void FloatVector_rand_from_(struct FloatVector* this, struct FloatVector value_set);
+
+void FloatVector_fill_(struct FloatVector* this, Float value);
+
+void FloatVector_range_(struct FloatVector* this, Float start, Float step, Float stop);
+
+void FloatVector_copy_from_(struct FloatVector* this, struct FloatVector src);
+
+Float FloatVector_sum(struct FloatVector* this);
+
+Float FloatVector_prod(struct FloatVector* this);
+
+Float FloatVector_min(struct FloatVector* this);
+
+Float FloatVector_max(struct FloatVector* this);
+
+Int FloatVector_argmin(struct FloatVector* this);
+
+Int FloatVector_argmax(struct FloatVector* this);
+
+void FloatVector_cumsum_(struct FloatVector* this, Float initial);
+
+void FloatVector_cumprod_(struct FloatVector* this, Float initial);
+
+void FloatVector_sort_(struct FloatVector* this);
+
+void FloatVector_sortperm_(struct FloatVector* this, struct IntVector* perm);
+
+extern struct FloatVectorMethods _CBL_FLOAT_VECTOR_METHODS;
+
+inline static void FloatVector_new_(struct FloatVector* this) {
+    this->len = 0;
+    this->data = NULL;
+    this->methods = &_CBL_FLOAT_VECTOR_METHODS;
+}
+
+#endif // _CBL_FLOATVECTOR_H_
